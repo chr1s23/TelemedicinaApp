0e92481064329a4f702905751b8d7d5d44969692
JonnathanAguilar <jonnathan.aguilarl@ucuenca.edu.ec>
2025-06-10 15:16:34 -0500

    [ADD] Implementation of the maps (first part: Centro_Salud)

---
+++ lib/view/screens/maps_menu_screen.dart
@@ -0,0 +1,68 @@
+import 'package:chatbot/view/widgets/utils.dart';
+import 'package:flutter/material.dart';
+import 'maps_screen.dart';
+
+class MapsMenuScreen extends StatelessWidget {
+  const MapsMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text(
+            "Servicios de Salud",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: AllowedColors.black),
+          ),),
+      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        const MapsScreen(establecimiento: 'CENTRO_SALUD'),
+                  ),
+                );
+              },
+              child: const Text('Mapa - Centros de Salud'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MapsScreen(
+                        establecimiento: 'CENTRO_CONTRA_VIOLENCIA'),
+                  ),
+                );
+              },
+              child: const Text('Mapa - Centros contra la Violencia'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MapsScreen(
+                        establecimiento: 'CENTRO_ATENCION_PSICOLOGICA'),
+                  ),
+                );
+              },
+              child: const Text('Mapa - Centros de Atención Psicológica'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
