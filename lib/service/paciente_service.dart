2288abce934f1a65f9686148fed397b036044b12
JonnathanAguilar <jonnathan.aguilarl@ucuenca.edu.ec>
2025-06-10 15:08:36 -0500

    [REF] Update baseURL handling for multiple environments

--- lib/service/paciente_service.dart
+++ lib/service/paciente_service.dart
@@ -9,13 +9,14 @@ import 'package:chatbot/view/widgets/utils.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+import 'package:chatbot/config/env.dart'; // Cambio de ambientes
 
 final _log = Logger('PacienteService');
 Dio? _dio;
 
 Dio getDio() {
   _dio ??= Dio(BaseOptions(
-    baseUrl: "https://clias.ucuenca.edu.ec",
+    baseUrl: AppConfig.baseUrl,
     headers: {'Content-Type': 'application/json'},
   ));
 
