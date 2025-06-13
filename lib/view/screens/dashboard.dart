c1d6a79c049afde51b881ba9ce4ae3e6d665d15f
JonnathanAguilar <jonnathan.aguilarl@ucuenca.edu.ec>
2025-06-13 11:39:49 -0500

    [REF] Replace MapsMenuScreen with MapsUnifiedScreen in main screen body

--- lib/view/screens/dashboard.dart
+++ lib/view/screens/dashboard.dart
@@ -2,7 +2,7 @@ import 'package:chatbot/model/storage/storage.dart';
 import 'package:chatbot/utils/connectivity_listener.dart';
 import 'package:chatbot/view/screens/chat.dart';
 import 'package:chatbot/view/screens/form_chat.dart';
-import 'package:chatbot/view/screens/maps_menu_screen.dart';
+import 'package:chatbot/view/screens/maps_unified_screen.dart';
 //import 'package:chatbot/view/screens/notifications.dart';
 import 'package:chatbot/view/screens/resources.dart';
 import 'package:chatbot/view/screens/scanner.dart';
@@ -105,9 +105,8 @@ class _AutoSamplingPageState extends State<Dashboard> {
       body: <Widget?>[
         _buildBody(),
         Resources(),
-        const MapsMenuScreen(), // Pantalla de mapas
-        WIPScreen(),
-        //Notifications(),
+        const MapsUnifiedScreen(), // Pantalla de mapas
+        WIPScreen(), //Notifications(),
       ][_currentIndex],
       bottomNavigationBar: _buildBottomNavigationBar(
           (index) => _currentIndex = index, () => _currentIndex),
@@ -136,7 +135,7 @@ class _AutoSamplingPageState extends State<Dashboard> {
                 const SizedBox(height: 20),
                 CustomButton(
                     color: deviceRegistered
-                        ? AllowedColors.blue
+                        ? const Color(0xFF002856)
                         : AllowedColors.gray,
                     label: "Iniciar proceso de Automuestreo",
                     onPressed: deviceRegistered
