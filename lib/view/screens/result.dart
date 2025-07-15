8e80ec59ef554937f6fd52c0ac26f49b16ed92b6
Emily Romero <romero.emily.225@gmail.com>
2025-07-15 12:24:46 -0500

    [ADD]: Download PDF

--- lib/view/screens/result.dart
+++ lib/view/screens/result.dart
@@ -3,6 +3,9 @@ import 'package:chatbot/view/screens/resultado_viewer.dart';
 import 'package:chatbot/view/widgets/custom_app_bar.dart';
 import 'package:chatbot/view/widgets/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:file_saver/file_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:typed_data';
 
 class Result extends StatelessWidget {
   final String nombrePaciente;
@@ -27,72 +30,128 @@ class Result extends StatelessWidget {
     }
   }
 
+  Future<void> descargarPDF(
+      Uint8List pdfBytes, String nombreArchivo, BuildContext context) async {
+    final status = await Permission.storage.request();
+
+    if (!status.isGranted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Permiso de almacenamiento denegado')),
+      );
+      return;
+    }
+
+    try {
+      await FileSaver.instance.saveFile(
+        name: nombreArchivo,
+        bytes: pdfBytes,
+        ext: "pdf",
+        mimeType: MimeType.pdf,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('PDF descargado exitosamente')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al guardar el archivo: $e')),
+      );
+    }
+  }
+
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: const CustomAppBar(),
-    body: SafeArea(
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return SingleChildScrollView(
-            padding: const EdgeInsets.all(20),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minHeight: constraints.maxHeight),
-              child: IntrinsicHeight(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Información del Examen",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Información del Examen",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const Divider(thickness: 1.5, height: 30),
-                    _buildInfoItem("Paciente", nombrePaciente),
-                    const SizedBox(height: 10),
-                    _buildInfoItem(
-                        "Fecha del examen", resultado.fechaResultado ?? "Desconocida"),
-                    const SizedBox(height: 10),
-                    _buildInfoItem(
-                        "Diagnóstico", _descripcionDiagnostico(resultado.diagnostico)),
-                    const SizedBox(height: 30),
-                    Text(
-                      "En el siguiente apartado podrá visualizar su resultado completo en PDF.",
-                      style: TextStyle(fontSize: 14, color: AllowedColors.gray),
-                    ),
-                    const SizedBox(height: 15),
-                    Text(
-                      "Nombre del archivo: ${resultado.nombre}",
-                      style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 25),
-                    Center(
-                      child: ElevatedButton.icon(
-                        onPressed: () async {
-                          await Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => ResultadoViewer(resultado: resultado),
-                            ),
-                          );
-                        },
-                        icon: const Icon(Icons.picture_as_pdf),
-                        label: const Text("Ver resultado en PDF"),
+                      const Divider(thickness: 1.5, height: 30),
+                      _buildInfoItem("Paciente", nombrePaciente),
+                      const SizedBox(height: 10),
+                      _buildInfoItem("Fecha del examen",
+                          resultado.fechaResultado ?? "Desconocida"),
+                      const SizedBox(height: 10),
+                      _buildInfoItem("Diagnóstico",
+                          _descripcionDiagnostico(resultado.diagnostico)),
+                      const SizedBox(height: 30),
+                      Text(
+                        "En el siguiente apartado podrá visualizar su resultado completo en PDF.",
+                        style:
+                            TextStyle(fontSize: 14, color: AllowedColors.gray),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
+                      const SizedBox(height: 15),
+                      Text(
+                        "Nombre del archivo: ${resultado.nombre}",
+                        style: const TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 25),
+                      Center(
+                        child: ElevatedButton.icon(
+                          onPressed: () async {
+                            await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) =>
+                                    ResultadoViewer(resultado: resultado),
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.picture_as_pdf),
+                          label: const Text("Ver resultado en PDF"),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Center(
+                        child: ElevatedButton.icon(
+                          onPressed: () async {
+                            final pdfBytes = resultado.contenido;
+                            if (pdfBytes == null) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content:
+                                        Text('No se encontró el archivo PDF')),
+                              );
+                              return;
+                            }
+
+                            await descargarPDF(
+                              Uint8List.fromList(pdfBytes),
+                              resultado.nombre ?? 'resultado',
+                              context,
+                            );
+                          },
+                          icon: const Icon(Icons.download),
+                          label: const Text("Descargar PDF"),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
   Widget _buildInfoItem(String titulo, String contenido) {
     return Column(
